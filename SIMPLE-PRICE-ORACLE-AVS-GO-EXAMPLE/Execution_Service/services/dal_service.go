3d59eea1593e8ec0e79fd2d8ebc78e1d9897810a
himanshugarg06 <himanshug@othentic.xyz>
2025-01-15 15:16:56 +0530

    Fix signature

--- SIMPLE-PRICE-ORACLE-AVS-GO-EXAMPLE/Execution_Service/services/dal_service.go
+++ SIMPLE-PRICE-ORACLE-AVS-GO-EXAMPLE/Execution_Service/services/dal_service.go
@@ -1,13 +1,15 @@
 package services
 
 import (
-	"log"
 	"Execution_Service/config"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/ethereum/go-ethereum/common"
 	"encoding/hex"
+	"log"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/ethereum/go-ethereum/rpc"
 )
 
 func Init() {
@@ -15,11 +17,11 @@ func Init() {
 }
 
 type Params struct {
-    proofOfTask    string
-    data     string
+	proofOfTask      string
+	data             string
 	taskDefinitionId int
-    performerAddress string
-	signature string
+	performerAddress string
+	signature        string
 }
 
 func SendTask(proofOfTask string, data string, taskDefinitionId int) {
@@ -33,46 +35,52 @@ func SendTask(proofOfTask string, data string, taskDefinitionId int) {
 	performerAddress := crypto.PubkeyToAddress(wallet.PublicKey).Hex()
 
 	typeString, err := abi.NewType("string", "", nil)
-    typeAddress, err := abi.NewType("address", "", nil)
+	typeBytes, err := abi.NewType("bytes", "", nil)
+	typeAddress, err := abi.NewType("address", "", nil)
 	typeUint16, err := abi.NewType("uint16", "", nil)
-   
-    arguments := abi.Arguments{
-        {Type: typeString},
-        {Type: typeString},
-        {Type: typeAddress},
-        {Type: typeUint16},
-    }
 
-    dataPacked, err := arguments.Pack(proofOfTask, hex.EncodeToString([]byte(data)), common.HexToAddress(performerAddress), uint16(taskDefinitionId))
+	log.Println("values", proofOfTask, []byte(data), uint16(taskDefinitionId))
+	arguments := abi.Arguments{
+		{Type: typeString},
+		{Type: typeBytes},
+		{Type: typeAddress},
+		{Type: typeUint16},
+	}
+	dataBytes := []byte(data)
+
+	dataPacked, err := arguments.Pack(proofOfTask, dataBytes, common.HexToAddress(performerAddress), uint16(taskDefinitionId))
 	if err != nil {
 		log.Println("error occured while encoding")
 		log.Fatal(err)
 	}
-    
+
 	messageHash := crypto.Keccak256Hash(dataPacked)
-	
+
 	sig, err := crypto.Sign(messageHash.Bytes(), wallet)
 	if err != nil {
 		log.Fatal(err)
 	}
-	v := sig[64] + 27 // Ethereum adds 27 to v
-	serializedSignature := append(sig[:64], v) // Replace the last byte with the adjusted v
+	serializedSignature, err := rlp.EncodeToBytes(sig)
+	serializedSignatureHex := "0x" + hex.EncodeToString(serializedSignature)
 
-	signatureHex := "0x" + hex.EncodeToString(serializedSignature)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Send task to Ethereum
+	log.Println(serializedSignature)
+
 	client, err := rpc.Dial(config.OTHENTIC_CLIENT_RPC_ADDRESS)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	params := Params{
-        proofOfTask:    proofOfTask,
-        data: "0x" + hex.EncodeToString([]byte(data)),
-        taskDefinitionId: taskDefinitionId,
+		proofOfTask:      proofOfTask,
+		data:             "0x" + hex.EncodeToString([]byte(data)),
+		taskDefinitionId: taskDefinitionId,
 		performerAddress: performerAddress,
-		signature: signatureHex,
-    }
+		signature:        serializedSignatureHex,
+	}
 
 	response := makeRPCRequest(client, params)
 	log.Println("API response:", response)
